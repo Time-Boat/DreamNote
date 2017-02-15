915b9576f71f3caf92aa7b7bd4b2a54940b38e1d
liuh <770164810@qq.com>
2017-02-15 20:56:16 +0800

    个人信息模块的mvp搭建，recycleView中item的布局

---
+++ app/src/main/java/com/dreamnote/adapter/PersonalInfoAdapter.java
@@ -0,0 +1,25 @@
+package com.dreamnote.adapter;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.dreamnote.bean.DreamInfo;
+
+/**
+ * @author: Administrator
+ * @time 2017-02-15 10:23
+ * @email 770164810@qq.com
+ */
+
+public class PersonalInfoAdapter extends BaseQuickAdapter<DreamInfo, BaseViewHolder> {
+
+    public PersonalInfoAdapter() {
+        super(R.layout.item_personal_fragment, null);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder holder, DreamInfo item) {
+//        holder.setText(R.id.item_personal_name_img, item.getName())
+//                .setText(R.id.item_personal_time, item.getReleaseTime())
+//                .setText(R.id.item_personal_content, item.getContent());
+    }
+}
--- app/src/main/java/com/dreamnote/ui/main/personal/PersonalInfoFragment.java
+++ app/src/main/java/com/dreamnote/ui/main/personal/PersonalInfoFragment.java
@@ -10,8 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.dreamnote.R;
+import com.dreamnote.adapter.PersonalInfoAdapter;
 import com.dreamnote.bean.DreamInfo;
 
 import java.util.List;
@@ -35,6 +39,10 @@ public class PersonalInfoFragment extends BaseFragment<PersonalInfoContract.Pres
 
     LinearLayoutManager mLinearLayoutManager;
 
+    PersonalInfoAdapter mPersonalInfoAdapter;
+
+    private int pagination = 0;
+
     public static PersonalInfoFragment newInstance() {
         return new PersonalInfoFragment();
     }
@@ -51,26 +59,51 @@ public class PersonalInfoFragment extends BaseFragment<PersonalInfoContract.Pres
         View view = inflater.inflate(R.layout.fragment_personal, container, false);
         ButterKnife.bind(this, view);
         initStateBar(toolbar);
-        initData();
-        return view;
-    }
 
-    //沉降的initData方法是在懒加载中实现的，后期在做修改
-    private void initData() {
-//        mLinearLayoutManager = new LinearLayoutManager(_mActivity);
-//        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-//        mFooterLoading = LayoutInflater.inflate(R.layout.item_footer_loading, (ViewGroup) recyclerview.getParent(), false);
-//        mFooterNotLoading = getLayoutInflater(savedInstanceState).inflate(R.layout.item_footer_not_loading, (ViewGroup) recyclerview.getParent(), false);
-//        mFooterError = getLayoutInflater(savedInstanceState).inflate(R.layout.item_footer_error, (ViewGroup) recyclerview.getParent(), false);
-//        mFooterError.setOnClickListener(new View.OnClickListener() {
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         mTitle.setText("我");
+        initData();
         super.onViewCreated(view, savedInstanceState);
     }
 
+    //沉降的initData方法是在懒加载中实现的，后期在做修改
+    private void initData() {
+        mLinearLayoutManager = new LinearLayoutManager(_mActivity);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mFooterLoading = getLayoutInflater(null).inflate(R.layout.item_footer_loading, (ViewGroup) mRecyclerView.getParent(), false);
+        mFooterNotLoading = getLayoutInflater(null).inflate(R.layout.item_footer_not_loading, (ViewGroup) mRecyclerView.getParent(), false);
+        mFooterError = getLayoutInflater(null).inflate(R.layout.item_footer_error, (ViewGroup) mRecyclerView.getParent(), false);
+        mFooterError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(_mActivity,"点击FooterError重新加载",Toast.LENGTH_SHORT).show();
+                mPersonalInfoAdapter.removeAllFooterView();
+                mPersonalInfoAdapter.addFooterView(mFooterLoading);
+                mPresenter.queryData(pagination);
+            }
+        });
+        setAdapter();
+
+        mRecyclerView.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+                Toast.makeText(_mActivity,"点击Item:"+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mRecyclerView.setAdapter(mPersonalInfoAdapter);
+//        initPageStateLayout(pagestatelayout);
+//        initPtrFrameLayout(ptrframelayout);
+    }
+
+    private void setAdapter() {
+        mPersonalInfoAdapter = new PersonalInfoAdapter();
+        mPersonalInfoAdapter.removeAllFooterView();
+    }
 
     @Override
     public void refresh(List<DreamInfo> mDreamInfo, int pagination) {
